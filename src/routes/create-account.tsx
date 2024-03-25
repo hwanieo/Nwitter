2755c5db36f602555b62915c482c40868c8b14f1
hwnii <hwniieo@gmail.com>
2024-03-26 01:04:40 +0900

    feat: Github Log in

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,48 +1,17 @@
+import { FirebaseError } from 'firebase/app'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { ChangeEvent, FormEvent, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from '../components/auth-components'
+import GithubButton from '../components/github-button'
 import { auth } from '../firebase'
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0;
-`
-
-const Title = styled.h1`
-  font-size: 42px;
-`
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`
 
 export default function CreateAccount() {
   const navigate = useNavigate()
@@ -64,6 +33,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setError('')
 
     if (
       isLoading ||
@@ -80,11 +50,12 @@ export default function CreateAccount() {
         formData.email,
         formData.password
       )
-      console.log(credentials.user)
       await updateProfile(credentials.user, { displayName: formData.name })
       navigate('/')
     } catch (e) {
-      // Error
+      if (e instanceof FirebaseError) {
+        setError(e.message)
+      }
     } finally {
       setIsLoading(false)
     }
@@ -124,6 +95,10 @@ export default function CreateAccount() {
         />
       </Form>
       {error && <Error>{error}</Error>}
+      <Switcher>
+        Already have an account? <Link to='/login'>Log in &rarr;</Link>
+      </Switcher>
+      <GithubButton />
     </Wrapper>
   )
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,82 @@
+import { FirebaseError } from 'firebase/app'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from '../components/auth-components'
+import GithubButton from '../components/github-button'
+import { auth } from '../firebase'
+
 export default function Login() {
-  return <h1>login</h1>
+  const navigate = useNavigate()
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [error, setError] = useState('')
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError('')
+
+    if (isLoading || !formData.email.trim() || !formData.password.trim()) return
+
+    try {
+      setIsLoading(true)
+      await signInWithEmailAndPassword(auth, formData.email, formData.password)
+      navigate('/')
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name='email'
+          value={formData.email}
+          placeholder='Email'
+          type='email'
+          required
+        />
+        <Input
+          onChange={onChange}
+          name='password'
+          value={formData.password}
+          placeholder='Password'
+          type='password'
+          required
+        />
+        <Input type='submit' value={isLoading ? 'Loading...' : 'Log in'} />
+      </Form>
+      {error && <Error>{error}</Error>}
+      <Switcher>
+        Don't have an account?{' '}
+        <Link to='/create-account'>Create one &rarr;</Link>
+      </Switcher>
+      <GithubButton />
+    </Wrapper>
+  )
 }
