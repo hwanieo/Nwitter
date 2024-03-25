589fa24c8bd6577c6a390b89b9863bc0494d3bb8
hwnii <hwniieo@gmail.com>
2024-03-25 22:07:56 +0900

    fix: change firebaseConfig

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDD9lgr9Rp3FkKhNz3bi_42BUmvBly0uQI',
-  authDomain: 'nwitter-reloaded-cf102.firebaseapp.com',
-  projectId: 'nwitter-reloaded-cf102',
-  storageBucket: 'nwitter-reloaded-cf102.appspot.com',
-  messagingSenderId: '1016916510207',
-  appId: '1:1016916510207:web:34bbf58b33509889c4d3ca',
+  apiKey: 'AIzaSyBM6stZNlJr1UfQH28aHj6r1STV_Tu1LWk',
+  authDomain: 'nwitter-reloaded-3ff25.firebaseapp.com',
+  projectId: 'nwitter-reloaded-3ff25',
+  storageBucket: 'nwitter-reloaded-3ff25.appspot.com',
+  messagingSenderId: '248482805671',
+  appId: '1:248482805671:web:59e011ce3c61a73da6a882',
 }
 
 const app = initializeApp(firebaseConfig)
