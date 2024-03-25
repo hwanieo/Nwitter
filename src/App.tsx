b4d2dfcdc24482df1052fb608efce989c97469b7
hwnii <hwniieo@gmail.com>
2024-03-25 21:46:06 +0900

    feat: Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
 import Layout from './components/layout'
+import LoadingScreen from './components/loading-screen'
 import CreateAccount from './routes/create-account'
 import Home from './routes/home'
 import Login from './routes/login'
@@ -45,10 +47,19 @@ const GlobalStyles = createGlobalStyle`
 `
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const init = async () => {
+    // wait for firebase
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   )
 }
