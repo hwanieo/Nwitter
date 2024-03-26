4630a1ffdb4825f11257da4313b232841607891e
hwnii <hwniieo@gmail.com>
2024-03-26 19:48:03 +0900

    feat: Remove tweet

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,4 +1,8 @@
+import { deleteDoc, doc } from 'firebase/firestore'
+import { deleteObject, ref } from 'firebase/storage'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { auth, db, storage } from '../firebase'
 import { ITweet } from './Timeline'
 
 const Wrapper = styled.div`
@@ -31,12 +35,50 @@ const Payload = styled.p`
   font-size: 18px;
 `
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const user = auth.currentUser
+
+  const handleRemoteTweet = async () => {
+    if (!user || user.uid !== userId) return
+
+    try {
+      setIsLoading(true)
+      await deleteDoc(doc(db, 'tweets', id))
+
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`)
+        await deleteObject(photoRef)
+      }
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={handleRemoteTweet}>
+            {isLoading ? 'Loading...' : 'Delete'}
+          </DeleteButton>
+        ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
